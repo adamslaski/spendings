e82dd822f74ed7a86d13dc6b43228380f55223c5
Adam Slaski <adam.slaski@gmail.com>
2021-04-09 12:12:45 +0200

    fix message service

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,39 +1,53 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarRef, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
+  ref?: MatSnackBarRef<TextOnlySnackBar>;
+  callbacks: (() => MatSnackBarRef<TextOnlySnackBar>)[] = [];
   constructor(private snackBar: MatSnackBar) {}
 
-  info(message: string) {
-    console.log(message);
-    this.snackBar.open(message, '', {
+  private open(message: string, type: 'info' | 'warn' | 'error') {
+    const ref = this.snackBar.open(message, '', {
       duration: 5000,
       horizontalPosition: 'center',
       verticalPosition: 'top',
-      panelClass: ['info-message'],
+      panelClass: [`${type}-message`],
     });
+    ref.afterDismissed().subscribe((x) => {
+      if (this.callbacks.length > 0) {
+        const next = this.callbacks[0];
+        this.callbacks.splice(0, 1);
+        this.ref = next();
+      } else {
+        this.ref = undefined;
+      }
+    });
+    return ref;
+  }
+
+  private register(message: string, type: 'info' | 'warn' | 'error') {
+    if (!this.ref) {
+      this.ref = this.open(message, type);
+    } else {
+      this.callbacks.push(() => this.open(message, type));
+    }
+  }
+
+  info(message: string) {
+    console.log(message);
+    this.register(message, 'info');
   }
 
   warn(message: string) {
     console.warn(message);
-    this.snackBar.open(message, '', {
-      duration: 5000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-      panelClass: ['warn-message'],
-    });
+    this.register(message, 'warn');
   }
 
   error(message: string) {
     console.error(message);
-    this.snackBar.open(message, '', {
-      duration: 5000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-      panelClass: ['error-message'],
-    });
+    this.register(message, 'error');
   }
 }
