413a65628d8b6bc4a122c4137979f783d9e6dc4a
Adam Slaski <adam.slaski@gmail.com>
2021-04-13 09:37:06 +0200

    change empty record into empty tuple

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -10,7 +10,7 @@ type MessageType = 'info' | 'warn' | 'error';
 export class MessageService {
   ref?: MatSnackBarRef<TextOnlySnackBar>;
   private readonly messages: { message: string; type: MessageType }[] = [];
-  private readonly notification: Subject<Record<string, never>> = new Subject();
+  private readonly notification: Subject<[]> = new Subject();
   private token = true;
   constructor(private snackBar: MatSnackBar) {
     this.notification.subscribe((x) => {
@@ -33,14 +33,14 @@ export class MessageService {
     });
     ref.afterDismissed().subscribe((x) => {
       this.token = true;
-      this.notification.next({});
+      this.notification.next([]);
     });
     return ref;
   }
 
   private register(message1: string, type1: MessageType) {
     this.messages.push({ message: message1, type: type1 });
-    this.notification.next({});
+    this.notification.next([]);
   }
 
   info(message: string) {
